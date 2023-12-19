e3a352619d0ebd764667d3211ce4239f4c0397f4
Jenidu <122882412+Jenidu@users.noreply.github.com>
2023-12-19 11:42:21 +0100

    Seperated pool.js, customers.js & .env  from server.js

---
+++ server/pool.js
@@ -0,0 +1,18 @@
+const mariaDB = require('mariadb');
+require('dotenv').config();
+
+let pool
+
+try {
+    pool = mariaDB.createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 5});
+  }
+  catch (err) {
+    console.error("Error creating mariaDB pool:", err)
+}
+
+module.exports = pool;
